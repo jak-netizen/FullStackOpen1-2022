9271f0fdc7f66c88dde3a4f31ef5777e22e1bdf5
jak-netizen <114210665+jak-netizen@users.noreply.github.com>
2022-10-18 03:21:46 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,31 @@
+const App = () => {
+  const parts = []
+  const course = {
+   name: 'Half Stack application development',
+   parts: [
+    {
+    name: 'Fundamentals of React',
+    exercises: 10
+    },
+    {
+    name: 'Using props to pass data',
+   exercises:  7
+    },
+    {
+    name: 'State of a component',
+    exercises: 14
+    }
+   ]
+  }     
+  
+  
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={parts.course} />
+      <Total parts={parts} />
+    </div> 
+    )
+}
+
+export default App
